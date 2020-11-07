dc721d1f98204455e498dd79a8c82efff2434eaa
Abhishek Tyagi <abhishek4466@gmail.com>
2020-11-07 13:10:11 -0600

    Initial Commit

---
+++ Git-Commits/Git_CommitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_CommitsApp.swift
+//  Git-Commits
+//
+//  Created by Abhishek Tyagi on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Git_CommitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
