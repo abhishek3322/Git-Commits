497dfe3dbed5ed83e2453dcff760a733f0ea66ab
Abhishek Tyagi <abhishek4466@gmail.com>
2020-11-07 18:38:14 -0600

    Fetching commits from server using service

--- Git-Commits/Service/CommitService.swift
+++ Git-Commits/Service/CommitService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Alamofire
+
+class CommitService: ObservableObject {
+    
+    @Published var commits: [Commit] = []
+    
+    init() {
+        fetchLatest()
+    }
+    
+    func fetchLatest() {
+        let request = AF.request("https://api.github.com/repos/abhishek3322/Git-Commits/commits")
+        request.response { (response) in
+            guard let data = response.data else { return }
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+            
+            do {
+                let commits = try decoder.decode([Commit].self, from: data)
+                self.commits = commits
+            }catch {
+                print("error: ", error)
+            }
+        }
+    }
+}
